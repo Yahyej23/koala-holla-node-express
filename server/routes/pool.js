6eedb28a14753e098796348aae667a1a51069977
Yahye Mohamud <jyahye375@gmail.com>
2024-03-20 17:30:17 -0500

    done

--- server/routes/pool.js
+++ server/routes/pool.js
@@ -1,14 +1,12 @@
-// Import the required module
 const { Pool } = require("pg");
 
-// Configure the PostgreSQL connection pool
 const pool = new Pool({
-  user: "your_username", // Replace 'your_username' with your PostgreSQL username
-  host: "localhost", // Replace 'localhost' with your PostgreSQL host
-  database: "your_database", // Replace 'your_database' with your PostgreSQL database name
-  password: "your_password", // Replace 'your_password' with your PostgreSQL password
-  port: 5432, // Replace 5432 with your PostgreSQL port (default is 5432)
+  user: "your_username",
+  host: "localhost",
+  database: "your_database",
+  password: "your_password",
+  port: 5432,
 });
 
-// Export the pool instance to be used in other files
+// Export the pool
 module.exports = pool;
